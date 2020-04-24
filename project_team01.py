0d811ad0d0d0098e746bde08d1b1fd12e7a5ce53
Kai Windischman <kwindischman@gmail.com>
2020-04-24 16:49:33 -0500

    Finish setting up cache

--- project_team01.py
+++ project_team01.py
@@ -1,7 +1,22 @@
 import sys
 import math
-from .file import Block
 
+class Block:
+    def __init__(self, block_size, tag_size):
+        self.data = [0 for x in range(block_size)]
+        self.valid = True
+        self.tag = [tag_size]
+        self.tag_size = tag_size
+
+    def is_valid(self):
+        return self.valid
+
+    def set_valid(self, is_valid):
+        self.valid = is_valid
+
+    def set_tag(self, new_tag):
+        for i in range(tag):
+            tag[i] = 1 & (newtag >> tag_size-i)
 
 def print_args():
     RR = 'Round Robin'
@@ -57,7 +72,8 @@ def calculate_values():
     cost = (implementation_mem_size // 1024) * 0.05
 
 def set_cache():
-    global cache = [[Block(block_size, tag_size) for x in range(total_rows)] for y in range(associativity)]
+    global cache
+    cache = [[Block(block_size, tag_size) for x in range(total_rows)] for y in range(associativity)]
 
 def set_vars():
     global file_name
@@ -105,6 +121,7 @@ def main():
     calculate_values()
     print_calculated_values()
     print_addr()
+    set_cache()
 
 
 if __name__ == '__main__':
