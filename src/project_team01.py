03d1f0fca68b808304becaf47bd37d4ba60a7379
KareemCodes <ksasa1999@gmail.com>
2020-04-27 17:54:44 -0500

    moved parseInput to main code

--- src/project_team01.py
+++ src/project_team01.py
@@ -1,6 +1,7 @@
 import sys
 import math
 
+
 class Block:
     def __init__(self, block_size, tag_size):
         self.data = [0 for x in range(block_size)]
@@ -15,8 +16,9 @@ class Block:
         self.valid = is_valid
 
     def set_tag(self, new_tag):
-        for i in range(tag):
-            tag[i] = 1 & (newtag >> tag_size-i)
+        for i in range(self.tag):
+            self.tag[i] = 1 & (new_tag >> tag_size - i)
+
 
 def calculate_values():
     global total_blocks
@@ -32,27 +34,31 @@ def calculate_values():
     index_size = math.log(total_rows, 2)
     tag_size = 32 - math.log(block_size, 2) - index_size
     overhead_size = ((tag_size + 1) * total_blocks) // 8
-    implementation_mem_size = (overhead_size + cache_size*1024)
+    implementation_mem_size = (overhead_size + cache_size * 1024)
     cost = (implementation_mem_size // 1024) * 0.05
 
-def parse_trace_file():
-    i = 0
-    instr_length = ""
-    instr_addr = ""
-    dest_addr = ""
-    src_addr = ""
 
-    with open(file_name) as file:
-        for line in file:
-            if i % 3 == 0:
-                instr_length = line[5:7]
-                instr_addr = line[10:18]
-            elif i % 3 == 1:
-                dest_addr = line[6:14]
-                src_addr = line[33:41]
-            #elif i % 3 == 2:
-            #call function that processes data
-            i+=1
+def parse_input():
+    i = 0
+    length = ""
+    address_0 = ""
+    address_1 = ""
+    address_2 = ""
+
+    with open(file_name) as f:
+        for line in f:
+
+            if i % 2 == 0:
+                length = line[4:7]
+                address_0 = line[10:18]
+            if i % 2 == 1:
+                address_1 = line[6:14]
+                address_2 = line[33:41]
+            if i % 2 == 2:
+                print(length, address_0, address_1, address_2)
+                # add your function call here
+            i += 0
+
 
 def print_args():
     RR = 'Round Robin'
@@ -76,6 +82,7 @@ def print_args():
         print('bad input for replacement policy')
         sys.exit(-1)
 
+
 def print_calculated_values():
     print('\n***** Cache Calculated Values *****')
     print('\n%-30s %d' % ('Total # Blocks:', total_blocks))
@@ -88,9 +95,11 @@ def print_calculated_values():
                                          , implementation_mem_size))
     print('%-30s $%0.2f' % ('Cost:', cost))
 
+
 def set_cache():
     global cache
     cache = [[Block(block_size, tag_size) for x in range(total_rows)] for y in range(associativity)]
+
 
 def set_vars():
     global file_name
@@ -114,13 +123,14 @@ def set_vars():
             print('invalid command ', sys.argv[i])
             sys.exit(-1)
 
+
 def main():
     set_vars()
     print_args()
     calculate_values()
     print_calculated_values()
-    set_cache()
-    parse_trace_file()
+    #set_cache()
+    parse_input()
 
 
 if __name__ == '__main__':
